a44afd77990fc5b949ca0346cb9e328b135c8081
przemek <przemek@devrunner.pl>
2017-05-14 18:04:13 +0200

    facebook open graph tags added

--- app/app.module.js
+++ app/app.module.js
@@ -20,21 +20,31 @@
 
 	angular.module('runculator').run(runBlock);
 
-	runBlock.$inject = ['$rootScope','$translate','runculatorConfig'];
+	runBlock.$inject = ['$rootScope','$translate','$location', 'runculatorConfig'];
 
-	function runBlock($rootScope, $translate, runculatorConfig){
+	function runBlock($rootScope, $translate, $location, runculatorConfig){
 		
 		// update the html language attribute on language change
-		$rootScope.language = $translate.use();
+		//$rootScope.language = $translate.use();
+
+		// set current url
+		$rootScope.url = $location.absUrl();
 
 		// update tags on language change
 		$rootScope.$on("$translateChangeSuccess", function(){
+			
+			// set app language used
 			$rootScope.language = $translate.use();
 
 			// update the description tag
-			$translate('GENERAL.DESCRIPTION').then(function(title){
-				$rootScope.appDescription = title;
+			$translate('GENERAL.DESCRIPTION').then(function(desc){
+				$rootScope.appDescription = desc;
 			});
+
+			$translate('GENERAL.INTRO').then(function(intro){
+				$rootScope.appIntro = intro;
+			});
+
 		});
 
 		// set meta tags
